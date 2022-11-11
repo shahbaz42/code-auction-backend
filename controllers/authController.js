165a83273044bdbe46239a911248f3e5053f50f6
shahbaz42 <shahbaz_ali@outlook.in>
2022-11-11 18:22:53 +0530

    handle errors

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,82 +17,96 @@ exports.checkAdmin = (req, res, next) => {
 // -------------- authControllers --------------
 
 exports.register = async (req, res) => {
-    // to-do add captcha
-    const {
-        team_name,
-        password,
-        leader_name,
-        leader_email,
-        member_1_name,
-        member_1_email,
-        member_2_name,
-        member_2_email
-    } = req.body;
-    
-    const team = new Team({
-        team_name,
-        password,
-        leader_name,
-        leader_email,
-        member_1_name,
-        member_1_email,
-        member_2_name,
-        member_2_email
-    });
+    try {
+        // to-do add captcha
+        const {
+            team_name,
+            password,
+            leader_name,
+            leader_email,
+            member_1_name,
+            member_1_email,
+            member_2_name,
+            member_2_email
+        } = req.body;
 
-    //check if team already exists
-    const teamExists = await Team.findOne({ team_name });
-    if (teamExists) {
-        return res.status(400).json({
-            message: "Team already exists"
+        const team = new Team({
+            team_name,
+            password,
+            leader_name,
+            leader_email,
+            member_1_name,
+            member_1_email,
+            member_2_name,
+            member_2_email
+        });
+
+        //check if team already exists
+        const teamExists = await Team.findOne({ team_name });
+        if (teamExists) {
+            return res.status(400).json({
+                message: "Team already exists"
+            });
+        }
+
+        //check if leader email already exists
+        const leaderEmailExists = await Team.findOne({ leader_email });
+        if (leaderEmailExists) {
+            return res.status(400).json({
+                message: "Leader email already exists"
+            });
+        }
+
+        // else save team
+        const savedTeam = await team.save();
+        res.status(200).json({
+            message: "Team registered successfully",
+            team: savedTeam
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Something went wrong"
         });
     }
-
-    //check if leader email already exists
-    const leaderEmailExists = await Team.findOne({ leader_email });
-    if (leaderEmailExists) {
-        return res.status(400).json({
-            message: "Leader email already exists"
-        });
-    }
-
-    // else save team
-    const savedTeam = await team.save();
-    res.status(200).json({
-        message: "Team registered successfully",
-        team: savedTeam
-    });
 }
 
 // put Team mongodb id in jwt
 exports.login = async (req, res) => {
-    const { leader_email, password } = req.body;
+    try {
+        const { leader_email, password } = req.body;
 
-    // check if team exists
-    const team = await Team.findOne({ leader_email });
-    if (!team) {
-        return res.status(401).json({
-            message: "Leader email does not exist"
+        // check if team exists
+        const team = await Team.findOne({ leader_email });
+        if (!team) {
+            return res.status(401).json({
+                message: "Leader email does not exist"
+            });
+        }
+
+        // check if password is correct
+        const isPasswordCorrect = await bcrypt.compare(password, team.password);
+        if (!isPasswordCorrect) {
+            return res.status(401).json({
+                message: "Incorrect password"
+            });
+        }
+
+        // generate jwt
+        const token = jwt.sign({ team_id: team._id }, process.env.JWT_SECRET, {
+            expiresIn: "1d"
+        });
+
+        res.status(200).json({
+            message: "Login successful",
+            token
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Something went wrong"
         });
     }
-
-    // check if password is correct
-    const isPasswordCorrect = await bcrypt.compare(password, team.password);
-    if (!isPasswordCorrect) {
-        return res.status(401).json({
-            message: "Incorrect password"
-        });
-    }
-
-    // generate jwt
-    const token = jwt.sign({ team_id: team._id }, process.env.JWT_SECRET, {
-        expiresIn: "1d"
-    });
-
-    res.status(200).json({
-        message: "Login successful",
-        token
-    });
 }
 
 exports.logout = async (req, res) => {
