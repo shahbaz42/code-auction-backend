c59c10f35cb58c6ce10fdf0c38e085df7ace42e9
shahbaz42 <shahbaz_ali@outlook.in>
2022-11-12 00:13:52 +0530

    add question controller

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -1,32 +1,140 @@
-exports.sendQuestions = (req, res) => {
-    if (typeof(req.params.id) === 'undefined'){
-        res.send("should send all questions, without test_case output.")
+const Question = require("../models/Question");
+
+exports.sendQuestions = async (req, res) => {
+    if (typeof (req.params.id) === 'undefined') {
+        try {
+            const questions = await Question.find();
+            res.status(200).json({
+                questions
+            });
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({
+                message: "Something went wrong"
+            });
+        }
     } else {
-        const id = req.params.id;
-        res.send(`should send question id ${id}`);
+        try {
+            const id = req.params.id;
+            const question = await Question.findById(id);
+            res.status(200).json({
+                question
+            });
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({
+                message: "Something went wrong"
+            });
+        }
     }
 }
 
-exports.createQuestion = (req, res) => {
-    res.send("This route should create a new question.")
+exports.createQuestion = async (req, res) => {
+    const {
+        name,
+        description,
+        img_url,
+        test_case,
+        test_case_output,
+        difficulty,
+        base_price
+    } = req.body;
+
+    const question = new Question({
+        name,
+        description,
+        img_url,
+        test_case,
+        test_case_output,
+        difficulty,
+        base_price,
+    })
+
+    const saved_question = await question.save();
+
+    if (!saved_question) {
+        return res.status(500).json({
+            message: "Something went wrong"
+        });
+    }
+
+    res.status(200).json({
+        message: "Question created successfully",
+        question: saved_question
+    });
 }
 
-exports.updateQuestion = (req, res) => {
-    res.send("This route should update a question.")
+exports.updateQuestion = async (req, res) => {
+    // this is the route for the admin to update one field of the question
+    try {
+        const id = req.params.id;
+        const field = req.body.field;
+        const value = req.body.value;
+
+        //check if the field is valid
+        const valid_fields = ["name", "description", "img_url", "test_case", "test_case_output", "difficulty", "base_price", "status"];
+        if (!valid_fields.includes(field)) {
+            return res.status(400).json({
+                message: "Invalid field"
+            });
+        }
+
+        // update the field
+        const updated_question = await Question.findByIdAndUpdate(id, {
+            [field]: value
+        }, {
+            new: true
+        });
+
+        if (!updated_question) {
+            return res.status(500).json({
+                message: "Question not found"
+            });
+        }
+
+        res.status(200).json({
+            message: "Question updated successfully",
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Something went wrong"
+        });
+    }
 }
 
-exports.deleteQuestion = (req, res) => {
-    res.send("This route should delete a question.")
+exports.deleteQuestion = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const deleted_question = await Question.findByIdAndDelete(id);
+
+        if (!deleted_question) {
+            return res.status(500).json({
+                message: "Question not found"
+            });
+        }
+
+        res.status(200).json({
+            message: "Question deleted successfully",
+            question: deleted_question
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Something went wrong"
+        });
+    }
 }
 
 exports.submitSolution = (req, res) => {
-    res.send("This route should submit a solution.")
 }
 
 exports.getBids = (req, res) => {
-    res.send("This route should send all bids of a question.")
 }
 
 exports.placeBid = (req, res) => {
-    res.send("This route should place a bid.")
+
+
 }
--- routes/questionRouter.js
+++ routes/questionRouter.js
@@ -5,58 +5,6 @@ const { validateRequest } = require('../utils/validator');
 const questionController = require('../controllers/questionController');
 const authController = require('../controllers/authController')
 
-// --------------------------- Question Routes -------------------------------
-
-//  /questions
-router.get("/:id?", questionController.sendQuestions);
-
-router.post("/", 
-    authController.authMiddleware,
-    authController.checkAdmin,
-    [
-        body("name").exists().withMessage("Name is required"),
-        body("description").exists().withMessage("Description is required"),
-        body("test_case").exists().withMessage("Test case is required."),
-        body("difficulty").exists().withMessage("Difficulty ('easy', 'medium', 'hard') is required."),
-        body("base_price").exists().withMessage("Base price is required.")
-    ],
-    validateRequest,
-    questionController.createQuestion
-); 
-
-// this is the route for the admin to update one field of the question
-router.patch("/:id",
-    authController.authMiddleware,
-    authController.checkAdmin,
-    [
-        param("id").exists().withMessage("Question id is required"),
-        body("field").exists().withMessage("Field to be updated is required"),
-        body("value").exists().withMessage("Value to be updated is required")
-    ],
-    validateRequest,
-    questionController.updateQuestion
-);
-
-router.delete("/:id",
-    authController.authMiddleware,
-    authController.checkAdmin,
-    [
-        param("id").exists().withMessage("Question id is required")
-    ],
-    validateRequest,
-    questionController.deleteQuestion
-);
-
-router.post("/:id/submit",
-    authController.authMiddleware,
-    [
-        param("id").exists().withMessage("Question id is required"),
-        body("attempted_solution").exists().withMessage("Attempted solution is required")
-    ],
-    validateRequest,
-    questionController.submitSolution
-);
-
 // ----------------------------Auction Routes --------------------------------
 
 router.get("/:id/get_bids", questionController.getBids);
