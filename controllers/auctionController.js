d6a7a1706df1244fe2e3621049185a56a7e7b879
shahbaz42 <shahbaz_ali@outlook.in>
2022-11-12 06:14:27 +0530

    add auction routes and controller

---
+++ controllers/auctionController.js
@@ -0,0 +1,128 @@
+const Question = require("../models/Question");
+const Team = require("../models/Team");
+
+exports.getAvailableQuestions = async (req, res) => {
+    try {
+        // all questions except private
+        const questions = await Question.find({status: {$in: ["bidding", "sold", "unsold"]}})
+            .select("name description difficulty base_price status");
+        res.status(200).json({
+            questions
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: error.message
+        });
+    }
+}
+
+exports.getOneQuestion = async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const question = await Question.find({_id: id, status: {$in: ["bidding", "sold", "unsold"]}})
+            .select("-test_case_output");
+
+        res.status(200).json({
+            question
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: error.message
+        });
+    }
+}
+
+exports.getBids = async (req, res) => {
+    try {
+        const id = req.params.id;
+        // populate the bids array with team name
+        const question = await Question.findById(id).populate({
+            path: "bids.bid_by",
+            select: "team_name -_id",
+        });
+
+        if (!question) {
+            return res.status(500).json({
+                message: "Question not found"
+            });
+        }
+        const bids = question.bids;
+        res.status(200).json({
+            message: "Bids fetched successfully",
+            bids
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: error.message
+        });
+    }
+}
+
+exports.placeBid = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const bid_price = parseInt(req.body.bid_price);
+        const team = req.team;
+
+        const question = await Question.findById(id);
+        if (!question) {
+            return res.status(404).json({
+                message: "Question not found",
+            });
+        }
+
+        if (question.status === "sold") {
+            return res.status(400).json({
+                message: "Question already sold",
+            });
+        }
+
+        if (question.status === "private") {
+            return res.status(400).json({
+                message: "Question not available for bidding yet",
+            });
+        }
+
+        const top_bid = question.bids[0];
+
+        if (bid_price > team.balance) {
+            return res.status(400).json({
+                message: "Insufficient balance",
+            });
+        }
+
+        // if there is no top bid, then the bid price should be greater than base price
+        if (!top_bid && question.base_price >= bid_price) {
+            return res.status(400).json({
+                message: "Bid price should be greater than base price",
+            });
+        }
+
+        if (top_bid && top_bid.bid_price >= bid_price) {
+            return res.status(400).json({
+                message: "Bid price should be greater than top bid",
+            });
+        }
+
+        const bid = {
+            bid_by: team._id,
+            bid_price,
+        };
+
+        question.bids.unshift(bid);
+        await question.save();
+
+        return res.status(200).json({
+            message: "Bid placed successfully",
+        });
+    } catch (err) {
+        console.log(err)
+        return res.status(400).json({
+            message: err.message,
+        });
+    }
+};
--- routes/auctionRouter.js
+++ routes/auctionRouter.js
@@ -2,21 +2,24 @@ const express = require('express');
 const router = express.Router();
 const { body, query, param } = require('express-validator');
 const { validateRequest } = require('../utils/validator');
-const questionController = require('../controllers/adminController');
 const authController = require('../controllers/authController')
+const auctionController = require('../controllers/auctionController');
 
 // ----------------------------Auction Routes --------------------------------
 
-router.get("/:id/get_bids", questionController.getBids);
+router.get("/question", auctionController.getAvailableQuestions);
 
-router.post("/:id/place_bid",
-    authController.authMiddleware,
+router.get("/question/:id", auctionController.getOneQuestion);
+
+router.get("/question/:id/get_bids", auctionController.getBids);
+
+router.post("/question/:id/place_bid",
     [
         param("id").exists().withMessage("Question id is required"),
         body("bid_price").exists().withMessage("Bid price is required")
     ],
     validateRequest,
-    questionController.placeBid
+    auctionController.placeBid
 );
 
 module.exports = router
