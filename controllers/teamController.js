9fd62bb6dd83086d676ea23c2c053cde0c5794fd
shahbaz42 <shahbaz_ali@outlook.in>
2022-11-12 06:47:19 +0530

    add /team route

--- controllers/teamController.js
+++ controllers/teamController.js
@@ -1,8 +1,38 @@
-exports.sendTeamsData = (req, res, next) =>{
+const Team = require('../models/Team');
+
+// to-do add pagination
+exports.sendTeamsData = async (req, res, next) => {
     const id = req.params.id
-    if (typeof(id) === 'undefined') {
-        res.send("If id params is not provided then this route sends all teams")
+    if (typeof (id) === 'undefined') {
+        try {
+            const teams = await Team.find({}).populate({
+                path: 'assigned_questions.question_id',
+                select: 'name description difficulty'
+            }).select('-password -assigned_questions.question_id.test_case_output -assigned_questions.submittions -__v');
+
+            res.status(200).json({
+                teams
+            })
+        } catch (err) {
+            res.status(500).json({
+                message: err.message
+            })
+        }
     } else {
-        res.send(`This route will provide the details of team ${id}`)
+        try {
+            const team = await Team.findById(id)
+                .populate({
+                    path: 'assigned_questions.question_id',
+                    select: 'name description difficulty'
+                }).select('-password -assigned_questions.question_id.test_case_output -assigned_questions.submittions -__v');
+
+            res.status(200).json({
+                team
+            })
+        } catch (err) {
+            res.status(500).json({
+                message: err.message
+            })
+        }
     }
 }
