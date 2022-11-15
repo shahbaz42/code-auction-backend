f95c71c9e385a55e82c3435dfc5181ebe83d64e1
shahbaz42 <shahbaz_ali@outlook.in>
2022-11-15 12:38:26 +0530

    add code to fetch transactions

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,3 +1,5 @@
+const Transaction = require("../models/Transactions");
+
 exports.serverStatus = (req, res, next) => {
     res.status(200).send("Server is up and running.")
 }
@@ -6,6 +8,19 @@ exports.leaderboard = (req, res, next) => {
     res.send("Leaderboard");
 }
 
-exports.transactions = (req, res, next) => {
-    res.send("Transactions");
+exports.transactions = async(req, res, next) => {
+    try {
+        const transactions = await Transaction.find({})
+            .select("to from amount description")
+        if (!transactions) {
+            return res.status(500).json({
+                message: "Couldn't fetch transactions"
+            })
+        }
+        res.status(200).json(transactions)
+    } catch (error) {
+        res.status(500).json({
+            message : "Some error occured."
+        })
+    }
 }
