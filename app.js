b9c21fe9c6c9f59edb4f38129df4f82b2839f41f
shahbaz42 <shahbaz_ali@outlook.in>
2022-11-15 16:25:40 +0530

    update cors

--- app.js
+++ app.js
@@ -20,10 +20,15 @@ mongoose.connect(process.env.DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-.then(() => console.log("Connected to MongoDB"))
-.catch(err => console.log(err));
+    .then(() => console.log("Connected to MongoDB"))
+    .catch(err => console.log(err));
 
-app.use(cors());
+app.use(
+    cors({
+        origin: "*",
+    })
+);
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
