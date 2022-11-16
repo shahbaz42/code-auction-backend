b2cef54224c4b149400e2d29a6bbde8be46578dd
shahbaz42 <shahbaz_ali@outlook.in>
2022-11-17 01:58:23 +0530

    pagination in transactions controller

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -10,17 +10,49 @@ exports.leaderboard = (req, res, next) => {
 
 exports.transactions = async(req, res, next) => {
     try {
-        const transactions = await Transaction.find({})
-            .select("to from amount description")
-        if (!transactions) {
+        const page = parseInt(req.query.page);
+        const limit = parseInt(req.query.limit);
+        const startIndex = (page - 1) * limit;
+        const endIndex = page * limit;
+        const results = {};
+
+        if (page <= 0 || limit <= 0) {
+            return res.status(400).json({message : "Page and limit should be positive integers"});
+        }
+
+        if (endIndex < await Transaction.countDocuments().exec()) {
+            results.next = {
+                page: page + 1,
+                limit: limit
+            }
+        }
+
+        if (startIndex > 0) {
+            results.previous = {
+                page: page - 1,
+                limit: limit
+            }
+        }
+
+        results.transactions = await Transaction.find()
+            .populate("from", "team_name")
+            .sort({createdAt: -1})
+            .skip(startIndex)
+            .limit(limit);
+
+        results.total = await Transaction.countDocuments();
+
+        if (!results.transactions) {
             return res.status(500).json({
                 message: "Couldn't fetch transactions"
             })
         }
-        res.status(200).json(transactions)
+
+        res.status(200).json(results);
+
     } catch (error) {
         res.status(500).json({
-            message : "Some error occured."
+            message : error.message
         })
     }
 }
